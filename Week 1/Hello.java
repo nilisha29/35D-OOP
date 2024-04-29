79839fe6e481902a006fc8385fc665cb032f60bb
nilisha <nilishakhadgi29@gmail.com>
2024-04-29 14:18:21 +0545

    Java

--- Week 1/Hello.java
+++ Week 1/Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+         /* 1. Write a program that prints Hello World! to the console. Save the program as Hello.java.
+          */
+         System.out.println("Hello World");
     }
-}
+} 
